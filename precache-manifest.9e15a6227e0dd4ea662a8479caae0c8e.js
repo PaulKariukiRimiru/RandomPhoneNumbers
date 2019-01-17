b40a3cde2a0f869119e907d19732f309e6c634b3
Paul Kariuki <paulrimiru@gmail.com>
2019-01-17 10:24:17 +0300

    Updates

---
+++ precache-manifest.9e15a6227e0dd4ea662a8479caae0c8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee564a59893baf11b764",
+    "url": "/RandomPhoneNumbers/static/css/main.99a9ecd2.chunk.css"
+  },
+  {
+    "revision": "ee564a59893baf11b764",
+    "url": "/RandomPhoneNumbers/static/js/main.ee564a59.chunk.js"
+  },
+  {
+    "revision": "30df4db0cea93313c484",
+    "url": "/RandomPhoneNumbers/static/js/1.30df4db0.chunk.js"
+  },
+  {
+    "revision": "cdfbd99df71f099219e0",
+    "url": "/RandomPhoneNumbers/static/js/runtime~main.cdfbd99d.js"
+  },
+  {
+    "revision": "61d87a7d52377d48cc5399c16fe2c72d",
+    "url": "/RandomPhoneNumbers/index.html"
+  }
+];
