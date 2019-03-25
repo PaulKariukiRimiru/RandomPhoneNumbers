f1da217c0ed40de907251fa029af8da1693ed3ca
Paul Kariuki <paulrimiru@gmail.com>
2019-03-25 23:11:28 +0300

    feat(sort): add sort functionality

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -18,4 +18,12 @@ describe("The App component", () => {
   it("displays all numbers", () => {
     expect(wrapper.find('.listitem').length).toEqual(10000);
   });
+
+  it('sorts phonenumbers in descending order', () => {
+    const ascendingButton = wrapper.find('.button').at(1);
+
+    ascendingButton.simulate('click');
+
+    expect(wrapper.find('.listitem').at(0).text()).toEqual('0'+Math.min.apply(null, wrapper.instance().state.phonenumbers))
+  });
 });
